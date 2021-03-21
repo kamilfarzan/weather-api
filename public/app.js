e1095ddadaecab6473f1cfeaa031c2a15bd89742
Yakuza-69 <70583659+Yakuza-69@users.noreply.github.com>
2021-03-21 15:50:36 +0530

    working model, key.txt added

--- public/app.js
+++ public/app.js
@@ -1,45 +1,53 @@
-require("dotenv").config();
-const key = process.env.API_KEY;
+// GETTING THE KEY FROM key.txt
+const key = {};
+fetch("/resources/key.txt")
+  .then((e) => e.text())
+  .then((data) => {
+    key.pass = data;
+  });
 
+// GLOBAL VARIABLES
 const weather = {};
 weather.temperature = {
   unit: "°C",
 };
 const output = document.querySelector(".output");
 const KELVIN = 273;
-
 const input = document.querySelector(".inputkardo");
 const btn = document.querySelector("button");
+
+// BUTTON EVENT LISTENER
 btn.addEventListener(`click`, () => {
-  getWeather(input.value, key);
+  getWeather(input.value, key.pass);
 });
 
+// GET WEATHER FUNCTION
 function getWeather(input, key) {
-  console.log(input);
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${key}`;
-  console.log(api);
+
   fetch(api)
-    .then(function (response) {
+    .then((response) => {
       let data = response.json();
       return data;
     })
-    .then(function (data) {
+    .then((data) => {
       weather.city = data.name;
       weather.description = data.weather[0].main;
       weather.temperature.value = Math.floor(data.main.temp);
       weather.temperature.feelsLike = data.main.feels_like;
       weather.humidity = data.main.humidity;
       weather.country = data.sys.country;
-      console.log(data);
-      console.log(weather);
       displayWeather(weather);
     })
-    .catch(console.log("Error aa raha bhy"), errorMoment());
+    .catch(console.error("Error?"), displayError());
 }
 
+// DISPLAY WEATHER FUNCTION
 function displayWeather(e) {
   output.innerHTML = `City of ${e.city}, ${e.country}. ${e.description}. Humidity is at ${e.humidity}%. Temperature is ${e.temperature.value}°C and feels like ${e.temperature.feelsLike}°C.`;
 }
-function errorMoment() {
-  output.innerHTML = "";
+
+// ON ERROR FUNCTION
+function displayError() {
+  output.innerHTML = " ";
 }
--- server.js
+++ server.js
@@ -3,11 +3,20 @@ require("dotenv").config();
 const key = process.env.API_KEY;
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.use(express.json());
+// app.set("view options", { layout: false });
+// app.use(express.static(__dirname + "/public"));
+
+// app.get("/", function (req, res) {
+//   res.render("index.html");
+// });
+
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Started on ${port}`);
+app.use("/resources", express.static(__dirname + "/resources"));
+
+app.listen(3000, () => {
+  console.log(`Started on 3000`);
 });
+
+module.exports = key;
