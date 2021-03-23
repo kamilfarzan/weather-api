d1f1b8520f60e41a09e98cc3823be8a53589154a
Yakuza-69 <70583659+Yakuza-69@users.noreply.github.com>
2021-03-23 13:37:38 +0530

    seccs commit, working skycons

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const weather = {};
-<<<<<<< HEAD
 const date = new Date();
 const key = "976d88e8699f349aa3435ae965b66663";
 const city = document.querySelector("#search");
@@ -39,48 +38,10 @@ async function getWeather(city, unit, key) {
     weather.city = data.name;
     weather.country = data.sys.country;
     weather.feelsLike = Math.floor(data.main.feels_like);
-=======
-const date = new Date;
-// var d = new Date(2018, 11, 24, 10, 33, 30, 0);
-const key = "&appid=976d88e8699f349aa3435ae965b66663";
-
-const output = document.querySelector(".output");
-const city = document.querySelector("#city");
-const unit = document.querySelector("#unit")
-const btn = document.querySelector(".enterbtn");
-
-btn.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        getWeather(city.value, unit.value, key);
-    }
-});
-
-btn.addEventListener(`click`, (e) => {
-    e.preventDefault();
-    getWeather(city.value, unit.value, key);
-});
-
-async function getWeather(city, unit, key) {
-
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}${key}&units=${unit}`;
-  console.log(api);
-
-  try {
-    let response = await fetch(api);
-    let data = await response.json();
-    console.log(data)
-
-    weather.temperature = data.main.temp;
-    weather.description = data.weather[0].description;
-    weather.city = data.name;
-    weather.country = data.sys.country;
-    weather.feelsLike = data.main.feels_like;
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
     weather.humidity = data.main.humidity;
     weather.windSpeed = data.wind.speed;
     weather.sunrise = convertUNIX(data.sys.sunrise);
     weather.sunset = convertUNIX(data.sys.sunset);
-<<<<<<< HEAD
 
     // let currentDate = Math.round(date.getTime() / 1000);
 
@@ -101,46 +62,10 @@ async function getWeather(city, unit, key) {
     console.log(e);
     displayError();
   }
-=======
-    let currentDate = Math.round(date.getTime()/1000);
-
-    if (currentDate > data.sys.sunrise && currentDate < data.sys.sunset) {
-        weather.image = 'sun';
-    } else if (currentDate < data.sys.sunrise || currentDate > data.sys.sunset) {
-        weather.image = 'moon';
-    }
-
-    console.log(weather);
-    console.log(data);
-    displayWeather(weather);
-    }
-
-    catch (e) {
-        console.log(e);
-        displayError();
-    }
-}
-
-// DISPLAY PREFERRED UNIT
-
-let tempUnitDisplay = '';
-let windSpeedUnit = '';
-
-if (unit.value == 'metric') {
-    tempUnitDisplay = '°C';
-    windSpeedUnit = 'm/s'
-} else if (unit.value == 'imperial') {
-    tempUnitDisplay = '°F';
-    windSpeedUnit = 'miles/hour'
-} else {
-    tempUnitDisplay = 'K';
-    windSpeedUnit = 'm/s'
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
 }
 
 // CONVERT TIME FROM UNIX
 
-<<<<<<< HEAD
 function convertUNIX(timestamp) {
   let date = new Date(timestamp * 1000);
   let hours = date.getHours();
@@ -150,21 +75,10 @@ function convertUNIX(timestamp) {
     hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
 
   return formattedTime;
-=======
-convertUNIX = timestamp => {
-    let date = new Date(timestamp * 1000);
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let seconds = "0" + date.getSeconds();
-    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-    return formattedTime;
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
 }
 
 // FORMAT CURRENT DATE
 
-<<<<<<< HEAD
 function currentDateFormat(date) {
   const cDay = date.getDay();
   const cDate = date.getDate();
@@ -174,16 +88,6 @@ function currentDateFormat(date) {
   let day = "";
   let month = "";
   switch (cDay) {
-=======
-currentDateFormat = date => {    
-const cDay = date.getDay();
-const cDate = date.getDate();
-// const cHours = date.getHours();
-// const cMinutes = date.getMinutes();
-const cMonth = date.getMonth();
-
-switch (cDay) {
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
     case 0:
       day = "Sunday";
       break;
@@ -191,11 +95,7 @@ switch (cDay) {
       day = "Monday";
       break;
     case 2:
-<<<<<<< HEAD
       day = "Tuesday";
-=======
-       day = "Tuesday";
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
       break;
     case 3:
       day = "Wednesday";
@@ -210,7 +110,6 @@ switch (cDay) {
       day = "Saturday";
   }
 
-<<<<<<< HEAD
   switch (cMonth) {
     case 0:
       month = "Jan";
@@ -252,49 +151,6 @@ switch (cDay) {
 
   const compiledDate = `${day}, ${cDate} ${month}`;
   return compiledDate;
-=======
-switch (cMonth) {
-    case 0:
-        month = "Jan";
-        break;
-    case 1:
-        month = "Feb";
-        break;
-    case 2:
-        month = "Mar";
-        break;
-    case 3:
-        month = "Apr";
-        break;
-    case 4:
-        month = "May";
-        break;
-    case 5:
-        month = "Jun";
-        break;
-    case 6:
-        month = "Jul";
-        break;
-    case 7:
-        month = "Aug";
-        break;
-    case 8:
-        month = "Sep";
-        break;
-    case 9:
-        month = "Oct";
-        break;
-    case 10:
-        month = "Nov";
-        break;
-    case 11:
-        month = "Dec";
-        break;
-}
-
-const compiledDate = `${day}, ${cDate} ${month}`
-return compiledDate;
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
 }
 
 const dateToday = currentDateFormat(date);
@@ -302,7 +158,6 @@ const dateToday = currentDateFormat(date);
 // DISPLAY WEATHER TO UI
 
 function displayWeather(e) {
-<<<<<<< HEAD
   // DISPLAY PREFERRED UNIT
   let tempUnitDisplay = "";
   let windSpeedUnit = "";
@@ -392,53 +247,3 @@ function sortSkycons(e) {
       break;
   }
 }
-=======
-    output.innerHTML = 
-    `<div class="cont-loc">
-        <div class="user-loc">
-            <p class = "location">${e.city}, ${e.country}</p>
-            <p class="dateToday">${dateToday}</p>
-        </div>
-        <div class="tempDisplay">
-        <figure class="icons">
-            <canvas id="icon" width="64" height="64"></canvas>
-        </figure>
-        <figure class="icons">
-            <canvas id="icon" width="64" height="64"></canvas>
-        </figure>
-            ${e.temperature} ${tempUnitDisplay}
-        </div>
-    </div>
-    <div class="info">
-        <div><span>Humidity<p>${e.humidity}%</span></div>
-        <div>Wind Speed<p>${e.windSpeed} ${windSpeedUnit}</span></div>
-        <div>Sunrise<p>${e.sunrise}</span></div>
-        <div>Sunset<p>${e.sunset}</span></div>
-    </div>
-    <div class="description"><p>${e.description}</p>
-    <p style="font-size: 1.5vw; margin-top: 1.5vh;">Feels like ${e.feelsLike}°</p></div>`;
-}
-
-
-// ERROR 
-function displayError() {
-    output.innerHTML = `<div class="error">Error 404</div>`;
-}
-
-// SKYCONS 
-
-let icons = new Skycons({"color": "white"});
-
-icons.set("icon", "clear-day");
-// icons.set("clear-night", Skycons.CLEAR_NIGHT);
-// icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
-// icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
-// icons.set("cloudy", Skycons.CLOUDY);
-// icons.set("rain", Skycons.RAIN);
-// icons.set("sleet", Skycons.SLEET);
-// icons.set("snow", Skycons.SNOW);
-// icons.set("wind", Skycons.WIND);
-// icons.set("fog", Skycons.FOG);
-
-icons.play();
->>>>>>> 581cd55c8a7621accf61e519d80f58d1fc3c5447
